a301f3c0c172a465881a1bdd24cae3ba15a210ef
samba427 <samriddhbag@gmail.com>
2026-05-27 20:10:40 +0530

    missing number

--- src/Arrays/missing_numbers.java
+++ src/Arrays/missing_numbers.java
@@ -1,20 +1,31 @@
 package Arrays;
 import java.util.Scanner;
-public class missing_numbers
+public class missing_numbers //an array of size 5 with one number missing from 1 to 6
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number");
+        System.out.println("Enter length of array");
         int n=sc.nextInt();
-        System.out.println(n);
-        sc.nextLine();
-        System.out.println("Enter a message");
-        String s=sc.nextLine();
-        System.out.println(s);
-        for(int i=0;i<s.length();i++)
+        int arr[]=new int[n];
+        System.out.println("Enter array elements");
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int flag=0;
+        int i;
+        for(i=1;i<=n;i++)
         {
-            System.out.print(s.charAt(i));
+            for(int j=0;j<n;j++)
+            {
+                if(arr[j]==i)
+                    flag++;
+            }
+            if(flag!=1)
+            {
+                System.out.println("the missing number is "+i);
+                return;
+            }
+            flag=0;
         }
     }
 }
