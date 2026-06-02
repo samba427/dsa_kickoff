5753bd750cfda6408910daecb72962ad3da82191
samba427 <samriddhbag@gmail.com>
2026-06-02 14:42:02 +0530

    fixed code for leader

--- src/Arrays/Leader_in_arr.java
+++ src/Arrays/Leader_in_arr.java
@@ -1,4 +1,29 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Leader_in_arr {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter length of arr");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int i;
+        for(i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+
+        int max=arr[n-1];
+        System.out.println(max);
+        for(i=n-2;i>=0;i--)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+                System.out.println(max);
+            }
+        }
+
+    }
 }
