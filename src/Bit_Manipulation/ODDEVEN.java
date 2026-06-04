bcca77c26f1b062a36396dd952dc57971b643208
samba427 <samriddhbag@gmail.com>
2026-06-04 18:19:12 +0530

    odd even by bit manipulation

---
+++ src/Bit_Manipulation/ODDEVEN.java
@@ -0,0 +1,16 @@
+package Bit_Manipulation;
+
+import java.util.Scanner;
+
+public class ODDEVEN
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        if((n&1)==1)
+            System.out.println("Odd");
+        else System.out.println("Even");
+    }
+}
